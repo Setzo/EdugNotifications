6c590b3383f2d2df52b64c7e8b554360123de97a
Wojciech Pruszak <setzo94@gmail.com>
2016-02-25 21:49:58 +0100

    Fix

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 mvn clean compile assembly:single
-mv target/Crawler-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/crawl.jar
+mv target/Crawler-0.0.1-SNAPSHOT-jar-with-dependencies.jar $HOME/crawl.jar
 crontab -l > mycron
 echo "*/5 * * * * java -jar $HOME/crawl.jar" >> mycron
 crontab mycron
